6d9375d799f10e1e1e1176c602d4795c0812b5ad
alexperezortuno <alexander.perez@sixbell.com>
2022-09-28 15:50:31 -0300

    playing with qrcode options

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import io
 
 import qrcode.image.svg
 from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.colormasks import RadialGradiantColorMask, SolidFillColorMask
-from qrcode.image.styles.moduledrawers import RoundedModuleDrawer, GappedSquareModuleDrawer, CircleModuleDrawer
+from qrcode.image.styles.colormasks import RadialGradiantColorMask, SolidFillColorMask, SquareGradiantColorMask, \
+    HorizontalGradiantColorMask, VerticalGradiantColorMask, ImageColorMask
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer, GappedSquareModuleDrawer, CircleModuleDrawer, \
+    VerticalBarsDrawer, HorizontalBarsDrawer
 from PIL import Image, ImageDraw
 
 factory = qrcode.image.svg.SvgPathImage
-code = "https://facebook.com"
+code = "WIFI:S:WIFI_NAME;T:WPA;P:WIFI_PASSWORD;H:;"
 
 qr_code = qrcode.QRCode(
     version=1,
@@ -19,6 +21,12 @@ qr_code = qrcode.QRCode(
 qr_code.add_data(code)
 qr_code.make(fit=True)
 
+img = qr_code.make_image()
+img.save("img_wifi.svg")
+
+qr_code_png = qr_code.make_image()
+qr_code_png.save("img_wifi.png")
+
 qr = qrcode.QRCode()
 qr.add_data(code)
 f = io.StringIO()
@@ -29,19 +37,54 @@ print(f.read())
 qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
 qr.add_data(code)
 
-img_1 = qr.make_image(image_factory=StyledPilImage,
-                      module_drawer=RoundedModuleDrawer())
-img_2 = qr.make_image(image_factory=StyledPilImage,
-                      color_mask=RadialGradiantColorMask(
-                          back_color=(216, 64, 168),
-                          center_color=(29, 25, 210),
-                          edge_color=(123, 44, 189),
-                      ))
-img_3 = qr.make_image(image_factory=StyledPilImage,
-                      module_drawer=GappedSquareModuleDrawer())
-img_1.save('img_1.png')
-img_2.save('img_2.png')
-img_3.save('img_3.png')
+img_rounded = qr.make_image(image_factory=StyledPilImage,
+                            module_drawer=RoundedModuleDrawer())
+
+img_gapped = qr.make_image(image_factory=StyledPilImage,
+                           module_drawer=GappedSquareModuleDrawer())
+
+img_circle = qr.make_image(image_factory=StyledPilImage,
+                           module_drawer=CircleModuleDrawer())
+
+img_vertical_bars = qr.make_image(image_factory=StyledPilImage,
+                                  module_drawer=VerticalBarsDrawer())
+
+img_horizontal_bars = qr.make_image(image_factory=StyledPilImage,
+                                    module_drawer=HorizontalBarsDrawer())
+
+img_solids = qr.make_image(image_factory=StyledPilImage,
+                           color_mask=SolidFillColorMask())
+img_radial = qr.make_image(image_factory=StyledPilImage,
+                           color_mask=RadialGradiantColorMask(
+                               back_color=(216, 64, 168),
+                               center_color=(29, 25, 210),
+                               edge_color=(123, 44, 189),
+                           ))
+img_square = qr.make_image(image_factory=StyledPilImage,
+                           color_mask=SquareGradiantColorMask(
+                               back_color=(255, 255, 255),
+                               center_color=(29, 25, 210),
+                               edge_color=(123, 44, 189),
+                           ))
+img_horizontal_gradient = qr.make_image(image_factory=StyledPilImage,
+                                        color_mask=HorizontalGradiantColorMask(
+                                            back_color=(255, 255, 255),
+                                            left_color=(29, 25, 210),
+                                            right_color=(123, 44, 189),
+                                        ))
+# img_color_mask = qr.make_image(image_factory=StyledPilImage,
+#                                module_drawer=ImageColorMask())
+
+img_rounded.save('img_rounded.png')
+img_radial.save('img_radial.png')
+img_gapped.save('img_gapped.png')
+img_circle.save('img_circle.png')
+img_vertical_bars.save('img_vertical_bars.png')
+img_horizontal_bars.save('img_horizontal_bars.png')
+img_solids.save('img_solids.png')
+img_square.save('img_square.png')
+img_horizontal_gradient.save('img_horizontal_gradient.png')
+# img_color_mask.save('img_color_mask.png')
 
 
 def style_eyes(img):
@@ -62,11 +105,14 @@ qr_eyes_img = qr.make_image(image_factory=StyledPilImage,
                             color_mask=SolidFillColorMask(back_color=(255, 255, 255),
                                                           front_color=(255, 110, 0)))
 
+qr_eyes_img.save('img_qr_eyes.png')
+
 qr_img = qr.make_image(image_factory=StyledPilImage,
                        module_drawer=CircleModuleDrawer(),
                        color_mask=SolidFillColorMask(front_color=(59, 89, 152)),
-                       embeded_image_path="./assets/facebook.png")
+                       embeded_image_path="./assets/wifi.png")
+qr_img.save('img_qr.png')
 
 mask = style_eyes(qr_img)
 final_img = Image.composite(qr_eyes_img, qr_img, mask)
-final_img.save('qr.png')
+final_img.save('img_masked.png')
